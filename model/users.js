f42177d8f733a89a467287b989cf4a82d7ed6b61
unknown <andyb6@gmail.com>
2021-04-27 16:41:36 -0600

    before branching

--- model/users.js
+++ model/users.js
@@ -1,3 +1,6 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
 const db = require('./db')
 const textFormats = require('../view/textFormats')
 
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const app = express()
 
-
 const intro = require('./routes/intro').introRouter
 const routes = require('./routes/room').roomRouter
 const accuse = require('./routes/accuse').accuseRouter
 const score = require('./routes/score').scoreRouter
 const user = require('./routes/user').userRouter
 
-
 app.use('/', user)
 app.use('/intro', intro)
 app.use('/rooms', routes)
 app.use('/accuse', accuse)
 app.use('/score', score)
-
 
 const PORT = process.env.PORT || 3000;
 
